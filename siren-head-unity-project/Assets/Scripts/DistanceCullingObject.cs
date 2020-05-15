f78d52d4a70b50fa4ff516634b5972d207dc6bdf
Lucas Borowiecki <baconomist@gmail.com>
2020-05-15 17:59:56 -0400

    Finally fixed grass shader and render order
    - Re-did grass shader
    - Fixed grass appearing behind opaque objects w/ render queue
    - Added grass to scene

--- siren-head-unity-project/Assets/Scripts/DistanceCullingObject.cs
+++ siren-head-unity-project/Assets/Scripts/DistanceCullingObject.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
 
+
 [RequireComponent(typeof(MeshRenderer))]
 public class DistanceCullingObject : MonoBehaviour
 {
+    public bool enableInEditor = false;
     public float cullDistance = 100;
 
     private MeshRenderer _meshRenderer;
@@ -15,7 +17,27 @@ public class DistanceCullingObject : MonoBehaviour
         _meshRenderer = GetComponent<MeshRenderer>();
     }
 
+    private void OnValidate()
+    {
+        _camera = Camera.main;
+        _meshRenderer = GetComponent<MeshRenderer>();
+        
+        #if UNITY_EDITOR
+            if(enableInEditor)
+                UpdateVisibility();
+            else
+            {
+                _meshRenderer.enabled = true;
+            }
+        #endif
+    }
+
     private void FixedUpdate()
+    {
+        UpdateVisibility();
+    }
+
+    void UpdateVisibility()
     {
         _meshRenderer.enabled = Vector3.Distance(_camera.transform.position, transform.position) < cullDistance;
     }
